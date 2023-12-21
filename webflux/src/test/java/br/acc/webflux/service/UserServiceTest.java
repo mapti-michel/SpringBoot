97457a3b62b2c60e3cc9bbff1cf4ce4376ec7e0a
Michel Pereira <michel.a.pereira@accenture.com>
2023-12-21 14:13:36 -0300

    Add test user save

---
+++ webflux/src/test/java/br/acc/webflux/service/UserServiceTest.java
@@ -0,0 +1,51 @@
+package br.acc.webflux.service;
+
+import br.acc.webflux.entity.User;
+import br.acc.webflux.mapper.UserMapper;
+import br.acc.webflux.model.request.UserRequest;
+import br.acc.webflux.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Objects;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private UserMapper mapper;
+
+    @InjectMocks
+    private UserService service;
+
+    @Test
+    void save() {
+        UserRequest request = new UserRequest("Michel", "12345678");
+
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class))).thenReturn(entity);
+        when(repository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
+
+        Mono<User> result = service.save(request);
+        StepVerifier.create(result)
+                .expectNextMatches(Objects::nonNull)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(2)).save(any(User.class));
+    }
+}
