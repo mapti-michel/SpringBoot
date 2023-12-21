e1cbfbf8f52df43dbc0731c056617aa9bd620b96
Michel Pereira <michel.a.pereira@accenture.com>
2023-12-21 16:13:59 -0300

    Add user findAll test

--- webflux/src/test/java/br/acc/webflux/service/UserServiceTest.java
+++ webflux/src/test/java/br/acc/webflux/service/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -65,4 +66,19 @@ class UserServiceTest {
 
         Mockito.verify(repository, times(1)).findById(anyLong());
     }
+
+    @Test
+    void testFindAll(){
+        when(repository.findAll()).thenReturn(Flux.just(User.builder().build()));
+        Flux<User> result = service.findAll();
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).findAll();
+
+    }
+
 }
